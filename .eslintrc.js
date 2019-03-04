26c0b93b7f5fa29a384cb3855859f13eb8b32df1
codact <savalijey@gmail.com>
2019-03-04 14:12:26 +0300

    Chore(Authors heaven) Set up an empty react app
     - use create-react-app tool to initiate an empty react project
     - set up eslint
    
    [Finishes #163388088]

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "semi": ["error", "always"],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "no-trailing-spaces": 0
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h3>Authors haven</h3>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
